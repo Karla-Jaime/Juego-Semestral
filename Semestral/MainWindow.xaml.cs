77127fd870e1d4dc5b7e415239f7de97af647f4d
Karly-98 <Karly-98@DESKTOP-D2ALSSH>
2018-11-20 17:03:27 -0700

    cambios imagenes

--- Semestral/MainWindow.xaml.cs
+++ Semestral/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+///Librerias para multiprocesamientos
+using System.Threading; //Se agrego esta libreria para Threading
+using System.Diagnostics; // Para el dispatcher y timers 
+
 
 namespace Semestral
 {
@@ -20,9 +24,28 @@ namespace Semestral
     /// </summary>
     public partial class MainWindow : Window
     {
+        Stopwatch stopwatch; //Toma el tiempo de ejecución del programa
+        TimeSpan tiempoAnterior; //Timespan guarda rangos de tiempo
+
+        enum EstadoJuego { GamePlay, GameOver, GameStart, GameRules, GameEnemies };
+        EstadoJuego estadoActual = EstadoJuego.GamePlay;
+
+        enum Direccion { Arriba, Abajo, Derecha, Izquierda, Ninguna };  //Para aclarar la direccion del jugador
+        Direccion direccionJugador = Direccion.Ninguna; //Inicializar
+
+        double velocidadEnem1 = 80;
+        double velocidadEnem2 = 80;
+        double velocidadMeteorito = 80;
+
         public MainWindow()
         {
             InitializeComponent();
+            canvasGamePlay.Focus();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
@@ -46,6 +69,95 @@ namespace Semestral
         {
             canvasEnemigos.Visibility = Visibility.Collapsed;
             canvasReglas.Visibility = Visibility.Visible;
+        }
+
+        private void canvasGamePlay_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (estadoActual == EstadoJuego.GamePlay)
+            {
+                if (e.Key == Key.Up)
+                {
+                    direccionJugador = Direccion.Arriba;
+                }
+                if (e.Key == Key.Down)
+                {
+                    direccionJugador = Direccion.Abajo;
+                }
+
+                if (e.Key == Key.Left)
+                {
+                    direccionJugador = Direccion.Izquierda;
+                }
+
+                if (e.Key == Key.Right)
+                {
+                    direccionJugador = Direccion.Derecha;
+                }
+                ///Con las teclas WASD{}
+                //                if (e.Key == Key.W)
+                {
+                    direccionJugador = Direccion.Arriba;
+                }
+
+                if (e.Key == Key.S)
+                {
+                    direccionJugador = Direccion.Abajo;
+                }
+
+                if (e.Key == Key.A)
+                {
+                    direccionJugador = Direccion.Izquierda;
+                }
+
+                if(e.Key == Key.D)
+                {
+                    direccionJugador = Direccion.Derecha;
+                }
+            }
+        }
+
+        private void canvasGamePlay_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up && direccionJugador == Direccion.Arriba)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
+            if (e.Key == Key.Down && direccionJugador == Direccion.Abajo)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
+            if (e.Key == Key.Left && direccionJugador == Direccion.Izquierda)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
+            if (e.Key == Key.Right && direccionJugador == Direccion.Derecha)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
+            ///Con las teclas WASD
+            if(e.Key == Key.W && direccionJugador == Direccion.Arriba)
+            {
+                direccionJugador = Direccion.Ninguna; 
+            }
+            if (e.Key == Key.S && direccionJugador == Direccion.Abajo)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
+            if (e.Key == Key.A && direccionJugador == Direccion.Izquierda)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
+            if (e.Key == Key.D && direccionJugador == Direccion.Derecha)
+            {
+                direccionJugador = Direccion.Ninguna;
+            }
+
         }
     }
 }
